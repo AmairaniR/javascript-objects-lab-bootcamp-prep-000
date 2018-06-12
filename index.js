5953278621e8da309352136d553549247781f80c
amarsr13686@gmail.com <amarsr13686@gmail.com>
2018-06-12 17:56:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {rice: '1 cup', water: '2 cups', salt: 'to taste'}
 function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, {[key]: value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue (recipes, key, value) {
+  return recipes.key = "value"
+}
