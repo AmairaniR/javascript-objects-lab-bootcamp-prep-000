256e7e917ac84f6f65a137ef726a938b22cbbb1a
amarsr13686@gmail.com <amarsr13686@gmail.com>
2018-06-12 17:49:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {rice: '1 cup', water: '2 cups', salt: 'to taste'}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({}, {key: value})
+  Object.assign({}, {rice: '2 cups'})
   return recipes
 }
