eddda4de0772bacc661e3cdc84c8167adab69264
amarsr13686@gmail.com <amarsr13686@gmail.com>
2018-06-12 18:20:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue (recipes, key, value) {
 
 function deleteFromObjectByKey(recipes, key) {
   var newRecipes = Object.assign({}, recipes)
-  delete newRecipes.key
   return delete newRecipes.key
 }
 
